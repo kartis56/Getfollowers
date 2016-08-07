b1700ecd5e74fb0c154517778ec1300041bf0248
U-octpati\tkuroda <tkuroda@dd.iij4u.or.jp>
2016-08-07 19:24:06 +0900

    change tab to space

--- get_followers.pl
+++ get_followers.pl
@@ -22,12 +22,12 @@ use Data::Dumper;
 use Net::Twitter::Lite::WithAPIv1_1;
 
 eval 'use Net::Twitter::Lite ; 1' or  # Twitter API用モジュール、ない場合はエラー表示
-	die "ERROR : cannot load Net::Twitter::Lite\n" ;
+    die "ERROR : cannot load Net::Twitter::Lite\n" ;
 eval 'use Encode ; 1' or              # 文字コード変換、ない場合はエラー表示
-	die "ERROR : cannot load Encode\n" ;
+    die "ERROR : cannot load Encode\n" ;
 use YAML::XS        'LoadFile';
 use Scalar::Util 'blessed';
-use IO::Handle;			#オートフラッシュ
+use IO::Handle;            #オートフラッシュ
 
 open TMP, '>>work.txt' ;
 
@@ -48,240 +48,234 @@ my $twit = Net::Twitter::Lite::WithAPIv1_1->new(%creds);
 #
 STDOUT->autoflush(1);
 while (<IN>) {
-	print STDERR $_ ,"\n";
-	get_followers_list($_);
+    print STDERR $_ ,"\n";
+    get_followers_list($_);
 }
 
 close TMP;
 exit ;
 
 
-
+=pod
 sub get_followers_list{
-	# IDリスト取得
-	my @ids = get_followers($_) ;
+    # IDリスト取得
+    my @ids = get_followers($_) ;
 
 }
-
+=cut
 # ====================
-sub get_followers {  # Usage: @ids = get_followers($screen_name) ;
-	my %arg ;
-	$arg{'screen_name'} = $_[0] ;  # API仕様として空白の場合は自分のフォロワー取得になるので注意
-	if ($debug == 1) { print " arg ".  $arg{'screen_name'} .' $_0 '. $_[0] ." --\n" ; }
-	
-	$arg{'cursor'} = -1 ;  # 1ページ目は -1 を指定
-	my @l_ids ;
-	my $ids_ref;
-	my $followers_ref;
+sub get_followers_list {  # Usage: @ids = get_followers($screen_name) ;
+    my %arg ;
+    $arg{'screen_name'} = $_[0] ;  # API仕様として空白の場合は自分のフォロワー取得になるので注意
+    if ($debug == 1) { print " arg ".  $arg{'screen_name'} .' $_0 '. $_[0] ." --\n" ; }
+    
+    $arg{'cursor'} = -1 ;  # 1ページ目は -1 を指定
+    my @l_ids ;
+    my $ids_ref;
+    my $followers_ref;
 
-	eval {
+    eval {
 
-	while ($arg{'cursor'}){ # 一度に5000までしか取得できないのでcursorを書き換えながら取得を繰り返す
+    while ($arg{'cursor'}){ # 一度に5000までしか取得できないのでcursorを書き換えながら取得を繰り返す
 
-		if ($debug == 1) { print " -- getfollowers call  --\n" ; }
-		wait_for_rate_limit('followers');
+        if ($debug == 1) { print " -- getfollowers call  --\n" ; }
+        wait_for_rate_limit('followers');
 
-		$followers_ref = $twit->followers_ids({%arg});
-			
-		$ids_ref = $followers_ref->{'ids'} ;
+        $followers_ref = $twit->followers_ids({%arg});
+            
+        $ids_ref = $followers_ref->{'ids'} ;
 
-		@l_ids = @{$ids_ref} ;
-		$arg{'cursor'} = $followers_ref->{'next_cursor'} ;
-		print STDERR "Fetched: ids=", scalar @$ids_ref, ",next_cursor=$arg{'cursor'}\n" ;
+        @l_ids = @{$ids_ref} ;
+        $arg{'cursor'} = $followers_ref->{'next_cursor'} ;
+        print STDERR "Fetched: ids=", scalar @$ids_ref, ",next_cursor=$arg{'cursor'}\n" ;
 
-# ユーザリストに変換
-# 100件ごとに分割して取得
-	TMP->autoflush(1);
-	while (my @ids_100 = splice(@l_ids,0,100)){
-		wait_for_rate_limit('lookup_users');
-	#	wait_for_rate_limit('friendships');
-		my @users = users_lookup(@ids_100) ;
-		$" = "\r\n" ;
-		print TMP "@users\r\n" ;
-	}
+        # ユーザリストに変換
+        # 100件ごとに分割して取得
+        TMP->autoflush(1);
+        while (my @ids_100 = splice(@l_ids,0,100)){
+          wait_for_rate_limit('lookup_users');
+          # wait_for_rate_limit('friendships');
+          my @users = users_lookup(@ids_100) ;
+          $" = "\r\n" ;
+          print TMP "@users\r\n" ;
+        }
 
-	}
+    }
 
-	}; #END of eval
-	if (my $err = $@) { 
-			die $@ unless blessed $err && $err->isa('Net::Twitter::Lite::WithAPIv1_1::Error');
+    }; #END of eval
+    if (my $err = $@) { 
+            warn "\n when followers_ids - HTTP Response Code: ", $err->code, "\n",
+            "\n - HTTP Message......: ", $err->message, "\n",
+            "\n - Twitter error.....: ", $err->error, "\n";
+            die $@ unless blessed $err && $err->isa('Net::Twitter::Lite::WithAPIv1_1::Error');
+    }
 
-			warn "\n when followers_ids - HTTP Response Code: ", $err->code, "\n",
-			"\n - HTTP Message......: ", $err->message, "\n",
-			"\n - Twitter error.....: ", $err->error, "\n";
-	
-	}
-
-	return  ;
+    return  ;
 } 
 
 # ====================
 sub users_lookup {  # usage: @userinfo = users_lookup(@user_id_list)
-	@_ or die "ERROR: users_lookup() : user_id_list is empty\n" ;
-	scalar (@_) <= 100 or die "ERROR: users_lookup() : user_id_list > 100\n" ;
+    @_ or die "ERROR: users_lookup() : user_id_list is empty\n" ;
+    scalar (@_) <= 100 or die "ERROR: users_lookup() : user_id_list > 100\n" ;
 
-	if ($debug == 1) { print " -- lookup call  --\n" ; }
-	my $user_id_list = join ',', @_ ;
-	my $user_ref;
-	my $rel_ref;
+    if ($debug == 1) { print " -- lookup call  --\n" ; }
+    my $user_id_list = join ',', @_ ;
+    my $user_ref;
+    my $rel_ref;
 
-	if ($debug == 1) { print " -- lookup call  lookup users--\n" ; }
-	eval {
-	$user_ref = $twit->lookup_users({ user_id => $user_id_list }) ;
-	};
-	
-		if (my $err = $@) { 
-				die $@ unless blessed $err && $err->isa('Net::Twitter::Lite::WithAPIv1_1::Error');
- 
-				warn "\n when lookup_ids - HTTP Response Code: ", $err->code, "\n",
-				"\n - HTTP Message......: ", $err->message, "\n",
-				"\n - Twitter error.....: ", $err->error, "\n";
-			
-		}
-	
-	if ($debug == 1) { print " -- lookup call  lookup friendships--\n" ; }
+    if ($debug == 1) { print " -- lookup call  lookup users--\n" ; }
+    eval {
+    $user_ref = $twit->lookup_users({ user_id => $user_id_list }) ;
+    };
+    
+        if (my $err = $@) { 
+                warn "\n when lookup_ids - HTTP Response Code: ", $err->code, "\n",
+                "\n - HTTP Message......: ", $err->message, "\n",
+                "\n - Twitter error.....: ", $err->error, "\n";
+                die $@ unless blessed $err && $err->isa('Net::Twitter::Lite::WithAPIv1_1::Error');
+        }
+    
+    if ($debug == 1) { print " -- lookup call  lookup friendships--\n" ; }
 
-	eval {
-	$rel_ref = $twit->lookup_friendships({ user_id => $user_id_list }) ;
-	};
-		if (my $err = $@) { 
-				die $@ unless blessed $err && $err->isa('Net::Twitter::Lite::WithAPIv1_1::Error');
- 
-				warn "\n when lookup_friendships - HTTP Response Code: ", $err->code, "\n",
-				"\n - HTTP Message......: ", $err->message, "\n",
-				"\n - Twitter error.....: ", $err->error, "\n";
-			
-		}
+    eval {
+    $rel_ref = $twit->lookup_friendships({ user_id => $user_id_list }) ;
+    };
+        if (my $err = $@) { 
+                warn "\n when lookup_friendships - HTTP Response Code: ", $err->code, "\n",
+                "\n - HTTP Message......: ", $err->message, "\n",
+                "\n - Twitter error.....: ", $err->error, "\n";
+                die $@ unless blessed $err && $err->isa('Net::Twitter::Lite::WithAPIv1_1::Error');
+        }
     my $i = 0;
-	my @user_info ;
-	my $ss;
+    my @user_info ;
+    my $ss;
 
-	$ss =  $rel_ref->[$i]->{'connections'};
-	#print "connections =  @{$ss[0]}  \n";
-	my $tmp =  join(",",  @{$ss});
-	if( $debug ==1 ) {print "connections =  $tmp \n" ; }
+    $ss =  $rel_ref->[$i]->{'connections'};
+    #print "connections =  @{$ss[0]}  \n";
+    my $tmp =  join(",",  @{$ss});
+    if( $debug ==1 ) {print "connections =  $tmp \n" ; }
 
-	foreach  (@$user_ref ) {
+    foreach  (@$user_ref ) {
 
-		my $screen_name       = $_->{'screen_name'}       // '' ;
+        my $screen_name       = $_->{'screen_name'}       // '' ;
 =pod
-		my $name              = $_->{'name'}              // '' ;
-		my $id              = $_->{'id'}              // '' ;
-		my $description       = $_->{'description'}       // '' ;
-		my $following          = $_->{'following'}        // '' ;
-		my $followers_count          = $_->{'followers_count'}        // '' ;
+        my $name              = $_->{'name'}              // '' ;
+        my $id              = $_->{'id'}              // '' ;
+        my $description       = $_->{'description'}       // '' ;
+        my $following          = $_->{'following'}        // '' ;
+        my $followers_count          = $_->{'followers_count'}        // '' ;
 =cut
-		my $protected          = $_->{'protected'}        // '' ;  #非公開アカウント
-		if ( $protected == 1 ) { $i++; next; }
-	
-		$ss          =   @{$rel_ref}[$i]->{'connections'}        // '' ;			# 関係性
+        my $protected          = $_->{'protected'}        // '' ;  #非公開アカウント
+        if ( $protected == 1 ) { $i++; next; }
+    
+        $ss          =   @{$rel_ref}[$i]->{'connections'}        // '' ;            # 関係性
 
-		my $dd = join(",",  @{$ss});
-		if ( $debug ==1) {
-			print "when $dd\n" ;
-		}
-		if ( $dd =~ /blocking/i ) { $i++; next; }		# block済みなら読み飛ばす
+        my $dd = join(",",  @{$ss});
+        if ( $debug ==1) {
+            print "when $dd\n" ;
+        }
+        if ( $dd =~ /blocking/i ) { $i++; next; }        # block済みなら読み飛ばす
 
-#		$name        =~ s/[\n\r\t]/ /g ;
-#		$description =~ s/[\n\r\t]/ /g ;
+#        $name        =~ s/[\n\r\t]/ /g ;
+#        $description =~ s/[\n\r\t]/ /g ;
 
-		my $userinfo = "$screen_name" ;
-		Encode::is_utf8($userinfo) and $userinfo = Encode::encode('utf-8',$userinfo) ;
+        my $userinfo = "$screen_name" ;
+        Encode::is_utf8($userinfo) and $userinfo = Encode::encode('utf-8',$userinfo) ;
 
-		push @user_info, $userinfo ;
-		$i++;
-	
-	}
-	if ( $@ ) { print "Error $@ \n"; }
-	
-	return @user_info ;
+        push @user_info, $userinfo ;
+        $i++;
+    
+    }
+    if ( $@ ) { print "Error $@ \n"; }
+    
+    return @user_info ;
 }
 
 #https://github.com/freebsdgirl/ggautoblocker/blob/master/ggautoblocker.pl  よりコピー改変
 # ====================
 sub get_rate_limit {
-	my $type = shift;
-	my $m ;
-	
-	eval{
-		$m = $twit->rate_limit_status;
-		print "App remaining ,". $m->{'resources'}->{'application'}->{'/application/rate_limit_status'}->{'remaining'} ."  \n";
-	};
+    my $type = shift;
+    my $m ;
+    
+    eval{
+        $m = $twit->rate_limit_status;
+        print "App remaining ,". $m->{'resources'}->{'application'}->{'/application/rate_limit_status'}->{'remaining'} ."  \n";
+    };
 
-	if ( my $err = $@ ) {
+    if ( my $err = $@ ) {
 
-		if ( $m->{'resources'}->{'application'}->{'/application/rate_limit_status'}->{'remaining'} == 0 ) {
-			if ($debug ==1) {
-				print "Zero remaining ". $m->{'resources'}->{'application'}->{'/application/rate_limit_status'}->{'remaining'} ."\n"; 
-				print " -- API limit reached, waiting for ". ( $m->{'resources'}->{'application'}->{'/application/rate_limit_status'}->{'reset'} - time ) . " seconds --\n" ;
-			}
-			
-			sleep ( $m->{'resources'}->{'application'}->{'/application/rate_limit_status'}->{'reset'} - time + 1 );
-		}
+        if ( $m->{'resources'}->{'application'}->{'/application/rate_limit_status'}->{'remaining'} == 0 ) {
+            if ($debug ==1) {
+                print "Zero remaining ". $m->{'resources'}->{'application'}->{'/application/rate_limit_status'}->{'remaining'} ."\n"; 
+                print " -- API limit reached, waiting for ". ( $m->{'resources'}->{'application'}->{'/application/rate_limit_status'}->{'reset'} - time ) . " seconds --\n" ;
+            }
+            
+            sleep ( $m->{'resources'}->{'application'}->{'/application/rate_limit_status'}->{'reset'} - time + 1 );
+        }
  
-		warn "when get_rate_limit  - HTTP Response Code: ", $err->code, "\n",
-		"\n - HTTP Message......: ", $err->message, "\n",
-		"\n - Twitter error.....: ", $err->error, "\n";
-		
-	}  # end $err 
-		
+        warn "when get_rate_limit  - HTTP Response Code: ", $err->code, "\n",
+        "\n - HTTP Message......: ", $err->message, "\n",
+        "\n - Twitter error.....: ", $err->error, "\n";
+        
+    }  # end $err 
+        
 
-	if ( $type =~ /followers/ ) {
-		print "followers remaining ,". $m->{'resources'}->{'followers'}->{'/followers/ids'}->{'remaining'} ."  \n";
-		return { 
-			type => $type,
-			remaining => $m->{'resources'}->{'followers'}->{'/followers/ids'}->{'remaining'}, 
-			reset => $m->{'resources'}->{'followers'}->{'/followers/ids'}->{'reset'} 
-		};
-	} else {
-	#if ( $type =~ /lookup_users/ ) {
-		my $user_look_rem;
-		my $friend_look_rem;
-		
-		$user_look_rem = $m->{'resources'}->{'users'}->{'/users/lookup'}->{'remaining'};
-		$friend_look_rem =  $m->{'resources'}->{'friendships'}->{'/friendships/lookup'}->{'remaining'};
-		
-		print "lookup_users remaining ,". $user_look_rem ."  \n";
-		print "follwers_ids remaining ,". $friend_look_rem ."  \n";
+    if ( $type =~ /followers/ ) {
+        print "followers remaining ,". $m->{'resources'}->{'followers'}->{'/followers/ids'}->{'remaining'} ."  \n";
+        return { 
+            type => $type,
+            remaining => $m->{'resources'}->{'followers'}->{'/followers/ids'}->{'remaining'}, 
+            reset => $m->{'resources'}->{'followers'}->{'/followers/ids'}->{'reset'} 
+        };
+    } else {
+    #if ( $type =~ /lookup_users/ ) {
+        my $user_look_rem;
+        my $friend_look_rem;
+        
+        $user_look_rem = $m->{'resources'}->{'users'}->{'/users/lookup'}->{'remaining'};
+        $friend_look_rem =  $m->{'resources'}->{'friendships'}->{'/friendships/lookup'}->{'remaining'};
+        
+        print "lookup_users remaining ,". $user_look_rem ."  \n";
+        print "follwers_ids remaining ,". $friend_look_rem ."  \n";
 
-		if( $user_look_rem  >= $friend_look_rem  ) {
-			return {
-				type => $type, 
-				remaining => $friend_look_rem,
-				reset => $m->{'resources'}->{'friendships'}->{'/friendships/lookup'}->{'reset'}
-			};
-		} else {
-			return {
-				type => $type, 
-				remaining => $user_look_rem,
-				reset => $m->{'resources'}->{'users'}->{'/users/lookup'}->{'reset'}
-			};
-		}
-	}
+        if( $user_look_rem  >= $friend_look_rem  ) {
+            return {
+                type => $type, 
+                remaining => $friend_look_rem,
+                reset => $m->{'resources'}->{'friendships'}->{'/friendships/lookup'}->{'reset'}
+            };
+        } else {
+            return {
+                type => $type, 
+                remaining => $user_look_rem,
+                reset => $m->{'resources'}->{'users'}->{'/users/lookup'}->{'reset'}
+            };
+        }
+    }
 
 }
 
 
 # ====================
 sub wait_for_rate_limit {
-	my $type = shift;
+    my $type = shift;
 
-	my $limit = get_rate_limit($type);
-		if ($debug ==1) { print "wait_for_rate_limit enter ". $limit->{'remaining'} ."\n"; }
-		
-	my $time = $limit->{'reset'} ;
-	
-	until ( $limit->{'remaining'} >= 2 ) {
-		if ($debug ==1) {
-			print STDERR " -- API limit reached in wait_for_limit, waiting for ". ( $time - time ) . " seconds -- type is : ". $type. "\n" ; 
-			print "----------------------- At until Loop\n";
-		}
-		sleep ( $time - time + 1 );
-		$limit = get_rate_limit($type);
-		$time = $limit->{'reset'} ;
-		if ( ($time - time) <= 0 ){		# resetが古いことがある
-			$time = time + 60; }
-		if ($debug ==1) { print STDERR "wait_for_rate_limit next Loop: ". $time . "  limit is : ". $limit->{'remaining'} ." type is : ". $type. "\n"; }
-	}
+    my $limit = get_rate_limit($type);
+        if ($debug ==1) { print "wait_for_rate_limit enter ". $limit->{'remaining'} ."\n"; }
+        
+    my $time = $limit->{'reset'} ;
+    
+    while ( $limit->{'remaining'} <= 2 ) {
+        if ($debug ==1) {
+            print STDERR " -- API limit reached in wait_for_limit, waiting for ". POSIX::strftime( "%Y/%m/%d %H:%M:%S", ( $time - time )) . " seconds -- type is : ". $type. "\n" ; 
+            print "----------------------- At until Loop\n";
+        }
+        sleep ( $time - time + 1 );
+        $limit = get_rate_limit($type);
+        $time = $limit->{'reset'} ;
+        if ( ($time - time) <= 0 ){ $time = time + 60; }        # resetが古いことがある
+
+        if ($debug ==1) { print STDERR "wait_for_rate_limit next Loop: ". $time . "  limit is : ". $limit->{'remaining'} ." type is : ". $type. "\n"; }
+    }
 
 }
