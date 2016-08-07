8c1d91faa49bb96a9694c903d3eb7b81f9d7d9dc
U-octpati\tkuroda <tkuroda@dd.iij4u.or.jp>
2016-08-07 19:19:32 +0900

    use Count

--- lib/MyApp/DB.pm
+++ lib/MyApp/DB.pm
@@ -4,5 +4,6 @@ use warnings;
 use parent "Teng";
 
 __PACKAGE__->load_plugin('+Teng::Plugin::FindOrCreate');
+__PACKAGE__->load_plugin(qw/Count/);
 #__PACKAGE__->load_plugin('+Teng::Plugin::SQLPager');
 1;
